eeee072283a68067c4ff68571c7c3071f1b0fa65
SLikhachev <aughing@hotmail.com>
2020-03-03 14:38:38 +1000

    notes table fixed

--- src/notes.sql
+++ src/notes.sql
@@ -4,6 +4,7 @@ id serial primary key,
 title varchar(127) NOT NULL,
 content text NOT NULL,
 created timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+completed timestamp,
 ddel smallint default 0
 
 )
